7a95deebcaebaff88bf0819419db7a252224caa2
Rin <a0492658817@gmail.com>
2025-10-07 21:33:21 +0800

    Update 15.py

--- 15.py
+++ 15.py
@@ -32,4 +32,34 @@ def main():
         sum+=balls[j]+balls[j+1]
 
     print(sum)
+
 main()
+"""
+same with this 
+def main():
+    balls=[]
+    sum=0
+    while True:
+        try:
+            line = input()
+            if line == "":  # 空行停止
+                break
+            balls.append(int(line))
+        except EOFError:
+            break
+    j=0
+    for i in range(3):
+        if(balls[j]==10):
+            sum+=10+balls[j+1]+balls[j+2]
+            #strike
+            j+=1
+        elif(balls[j]+balls[j+1]==10):
+            sum+=10+balls[j+2]
+            #spare
+            j+=2
+        else:
+            sum+=balls[j]+balls[j+1]
+            #normal
+            j+=2
+main()
+"""
